537a98ab79688ce4297e6ee60887f992d597321a
Your Name <you@example.com>
2022-10-07 16:08:09 -0700

    CSS

--- main.js
+++ main.js
@@ -1,37 +1,29 @@
 import './src/index.css';
 import Draggabilly from 'draggabilly';
 
-const button = document.getElementById('button');
+import $ from  'jquery';
+
+
 const elem = document.querySelector('#panzoomEl');
 
 function moveElem(){
+  var foo = document.getElementById('hotel-gallery-wrap')
   setInterval(function() {
-    var foo = document.getElementById('hotel-gallery-wrap')
+    
     //if (animation complete) clearInterval(timer);
-    foo.scrollTop = foo.scrollTop + 1;
+    foo.scrollTop = foo.scrollTop + 2;
     console.log("move...")
   }, 20)
 }
 
-button.onclick = () => {
-  document.getElementById('hotel-gallery-wrap').scrollLeft = 0;
-  document.getElementById('hotel-gallery-wrap').scrollTop = 0;
-  elem.classList.toggle('zoomed-out')
-  elem.style.top = 0
-  elem.style.left = 0
-  window.scrollTo(0, 0);
-  if (button.innerHTML === "Zoom out") {
-    button.innerHTML = "Zoom in";
-  } else {
-    button.innerHTML = "Zoom out";
-  }
-};
 
 
 function initDrag() {
-  
   var ww = window.innerWidth / 4;
-  document.getElementById('hotel-gallery-wrap').scrollLeft += ww;
+  var gWrap = document.getElementById('hotel-gallery-wrap')
+
+  gWrap.scrollTop = 0;  
+  gWrap.scrollLeft += ww;
 
   //var elem = document.querySelector('#panzoomEl');
   var draggie = new Draggabilly( elem, {
@@ -39,26 +31,51 @@ function initDrag() {
   });
 
   function listener(e) {
-    console.log( e, this.position.x, this.position.y );
-    
+    console.log( e, this.position.x, this.position.y );    
   }
 
   // bind event listener
-  draggie.on( 'dragStart', listener );
-  draggie.on( 'dragMove', listener );
-  draggie.on( 'dragEnd', listener );
-  draggie.on( 'pointerDown', listener );
-  draggie.on( 'pointerMove', listener );
-  draggie.on( 'pointerUp', listener );
-  draggie.on( 'staticClick', listener );
-  
-  // // unbind event listener
-  // draggie.off( 'dragEnd', listener );
-  
-  // // bind event listener to trigger once. note ONCE not ONE or ON
-  // draggie.off( 'dragStart', listener );
+  // draggie.on( 'dragStart', listener );
+  // draggie.on( 'dragMove', listener );
+  // draggie.on( 'dragEnd', listener );
+  // draggie.on( 'pointerDown', listener );
+  // draggie.on( 'pointerMove', listener );
+  // draggie.on( 'pointerUp', listener );
+  // draggie.on( 'staticClick', listener );
+
+  draggie.on( 'dragStart', function( event, pointer ){
+    console.log("Drag started... stop the auto scroll behaviour")
+  });
+
+  draggie.on( 'dragEnd', function( event, pointer ){
+    console.log("Drag ended... stop the auto scroll behaviour")
+  });  
+
+  draggie.on( 'staticClick', function( event, pointer ) {
+    alert("Static click..... trigger the Swiper overlay")
+    console.log(event.target)
+  })  
+
+  const button = document.getElementById('button');
+  button.onclick = () => {
+    document.getElementById('hotel-gallery-wrap').scrollLeft = 0;
+    document.getElementById('hotel-gallery-wrap').scrollTop = 0;
+    elem.classList.toggle('zoomed-out')
+    elem.style.top = 0
+    elem.style.left = 0
+    window.scrollTo(0, 0);
+    var ww = window.innerWidth / 4;
+    
+    if (button.innerHTML === "Zoom out") {
+      button.innerHTML = "Zoom in";
+      //draggie.disable()
+    } else {
+      button.innerHTML = "Zoom out";
+      document.getElementById('hotel-gallery-wrap').scrollLeft += ww;
+      //draggie.enable()
+    }
+  };
 
 }
-
-moveElem()
 initDrag()
+moveElem()
