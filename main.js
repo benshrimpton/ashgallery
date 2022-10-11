fbbcacf9632e2c738653448bebfc447b1a18ef31
Your Name <you@example.com>
2022-10-11 16:54:07 -0700

    JS and CSS

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
 import './src/index.css';
-import Draggabilly from 'draggabilly';
-
-import $ from  'jquery';
-
 
 const elem = document.querySelector('#panzoomEl');
-const button = document.getElementById('button');
-let theInterval;
 
+let theInterval;
 
 function scrollTimer() {
   var foo = document.getElementById('hotel-gallery-wrap')
-  foo.scrollTop = foo.scrollTop + 1;
-  // console.log(foo.scrollTop)
-  if( Math.floor(foo.scrollTop + 1) == foo.scrollTop) {
-    stopFunction()
-  }
+  var fooScrollheight =  foo.scrollHeight;
+  foo.scrollTop = Math.floor(foo.scrollTop + 1);
+  console.log(Math.floor(foo.scrollTop), fooScrollheight)
+  // if( Math.floor(foo.scrollTop + 1) == Math.floor(fooScrollheight)) {
+  //   stopFunction()
+  // }
 }
 
 function stopFunction() {
@@ -23,97 +19,120 @@ function stopFunction() {
   clearInterval(theInterval);
 }
 function startFunction() {
-  console.log("startFunction called")
-  theInterval = setInterval(scrollTimer, 24);
+  if($(window).width() > 1) {
+    console.log("startFunction called")
+    theInterval = setInterval(scrollTimer, 24);
+  }
 }
 
 
 function initDrag() {
-  var ww = window.innerWidth / 4;
-  var gWrap = document.getElementById('hotel-gallery-wrap')
-
-  gWrap.scrollTop = 0;  
-  gWrap.scrollLeft += ww;
-
-  //var elem = document.querySelector('#panzoomEl');
-  var draggie = new Draggabilly( elem, {
-    //containment: '#hotel-gallery-wrap'
-  });
-
-  function listener(e) {
-    console.log( e, this.position.x, this.position.y );    
+  //Click event for mobile
+  if($(window).width() < 768) {
+    var ww = window.innerWidth / 3;
+    var gWrap = document.getElementById('hotel-gallery-wrap')    
+    gWrap.scrollTop = 0;  
+    gWrap.scrollLeft += ww;
+    $(document).on('click', '.hotel-gallery__item', function(event){
+      triggerSwiperOverlay(event)
+    })
   }
-
-  // bind event listener
-  // draggie.on( 'dragStart', listener );
-  // draggie.on( 'dragMove', listener );
-  // draggie.on( 'dragEnd', listener );
-  // draggie.on( 'pointerDown', listener );
-  // draggie.on( 'pointerMove', listener );
-  // draggie.on( 'pointerUp', listener );
-  // draggie.on( 'staticClick', listener );
-
-  draggie.on( 'dragStart', function( event, pointer ){
-    console.log("Drag started... stop the auto scroll behaviour")
-    stopFunction()
-  });
-
-  draggie.on( 'dragEnd', function( event, pointer ){
-    console.log("Drag ended... stop the auto scroll behaviour");
-    setTimeout(function(){
-      startFunction();
-    }, 200)
+  //Code for Desktop
+  if($(window).width() > 1) {
+    var ww = window.innerWidth / 4;
+    var gWrap = document.getElementById('hotel-gallery-wrap')
     
-  });  
+    gWrap.scrollTop = 0;  
+    gWrap.scrollLeft += ww;
 
-  draggie.on( 'staticClick', function( event ) {
-    var parentElem = event.target.parentNode
-    if(parentElem.tagName == 'PICTURE') {
-      let item = parentElem.closest('.hotel-gallery__item')
-      alert("This will trigger the carousel overlay")
-    }    
-  })  
+    //var elem = document.querySelector('#panzoomEl');
+    var draggie = new Draggabilly( elem, {
+      //containment: '#hotel-gallery-wrap'
+    });
 
-  let count = 1;
-  
-  button.onclick = () => {
-    count = count++;
-    console.log(count)
-    document.getElementById('hotel-gallery-wrap').scrollLeft = 0;
-    document.getElementById('hotel-gallery-wrap').scrollTop = 0;
-    elem.classList.add('zoomed-out')
-    if( count < 4){
-      elem.classList.add(`zoomed-out-${count++}`)
-      elem.setAttribute('data-zoom', count++)
+    function listener(e) {
+      console.log( e, this.position.x, this.position.y );    
     }
-    else {
-      elem.classList.add(`zoomed-out-${--count}`)
-      elem.setAttribute('data-zoom', --count)
-    }
-    
-    //Set the XY position to top of page.
-    elem.style.top = 0
-    elem.style.left = 0
-    window.scrollTo(0, 0);
-    
-    
-    if (button.innerHTML === "Zoom out") {
-      button.innerHTML = "Zoom in";
-      stopFunction();
-      draggie.disable()      
-    }
-    else {
-      button.innerHTML = "Zoom out";
-      var ww = window.innerWidth / 4;
-      document.getElementById('hotel-gallery-wrap').scrollLeft += ww;
+
+    draggie.on( 'dragStart', function( event, pointer ){
+      console.log("Drag started... stop the auto scroll behaviour")
+      stopFunction()
+    });
+
+    draggie.on( 'dragEnd', function( event, pointer ){
+      console.log("Drag ended... stop the auto scroll behaviour");
       setTimeout(function(){
         startFunction();
-        draggie.enable()
       }, 200)
-            
-    }
-  };
+      
+    });  
 
+    draggie.on( 'staticClick', function( event ) {
+      triggerSwiperOverlay(event)
+        
+    }) 
+  }// END  if($(window).width() > 768) {
+
+  $(document).on('click', '#button', ()=> {
+  
+    //console.log(count, count++)
+    document.getElementById('hotel-gallery-wrap').scrollLeft = 0;
+    document.getElementById('hotel-gallery-wrap').scrollTop = 0;
+
+    if($("#panzoomEl").attr('data-zoom') === '0') {
+      $(this).addClass('zoom-out')
+      $("#panzoomEl").attr('data-zoom', '1')
+      $('#button-text').text( "Zoom out")
+      
+      stopFunction();
+      //Set the XY position to top of page.
+      elem.style.top = 0
+      elem.style.left = 0
+      window.scrollTo(0, 0);
+    }
+
+    else if($("#panzoomEl").attr('data-zoom') === '1') {
+      $(this).addClass('zoom-out')
+      $("#panzoomEl").attr('data-zoom', '2')
+      $('#button-text').text( "Zoom out")
+      stopFunction();
+      //Set the XY position to top of page.
+      elem.style.top = 0
+      elem.style.left = 0
+      window.scrollTo(0, 0);      
+    }    
+
+    else if($("#panzoomEl").attr('data-zoom') === '2') {
+      $("#panzoomEl").attr('data-zoom', '3')
+      $('#button-text').text( "Zoom in")
+      stopFunction();
+      //Set the XY position to top of page.
+      elem.style.top = 0
+      elem.style.left = 0
+      window.scrollTo(0, 0);      
+    } 
+
+    else if($("#panzoomEl").attr('data-zoom') === '3') {
+      $('#button-text').text( "Zoom out")
+      startFunction();   
+      var ww = window.innerWidth / 4;
+      var gWrap = document.getElementById('hotel-gallery-wrap')      
+      gWrap.scrollTop = 0;  
+      gWrap.scrollLeft += ww;
+      window.scrollTo(0, 0); 
+      $("#panzoomEl").attr('data-zoom', '0')        
+    } 
+  });
 }
+
+//Trigegr swiper overlay
+function triggerSwiperOverlay(event) {
+  var parentElem = event.target.parentNode
+  if(parentElem.tagName == 'PICTURE') {
+    let item = parentElem.closest('.hotel-gallery__item')
+    alert("This will trigger the carousel overlay")
+  } 
+}
+//START IT UP
 initDrag();
 startFunction();
